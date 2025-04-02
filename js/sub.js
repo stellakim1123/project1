2c5e5dadf8dda8bdc5498bfbe752387980ae38cc
stellakim1123 <glamstellakim@gmail.com>
2025-04-02 16:15:43 +0900

    2025.04.02

--- js/sub.js
+++ js/sub.js
@@ -15,3 +15,47 @@ $(document).ready(function(){
         $("img[alt='milk3']").show();
     });
 });
+
+$(document).ready(function(){
+    $("#detail2").hide();
+    $("#review").hide();
+    $("#inquiry").hide();
+    $("#shipping").hide();
+
+
+    $(".sub_nav li:first-of-type").click(function(){
+        $("#detail").show();
+        $("#detail2").hide();
+        $("#review").hide();
+        $("#inquiry").hide();
+        $("#shipping").hide();
+    });
+    $(".sub_nav li:nth-of-type(2)").click(function(){
+        $("#detail").hide();
+        $("#detail2").show();
+        $("#review").hide();
+        $("#inquiry").hide();
+        $("#shipping").hide();
+    });
+    $(".sub_nav li:nth-of-type(3)").click(function(){
+        $("#detail").hide();
+        $("#detail2").hide();
+        $("#review").show();
+        $("#inquiry").hide();
+        $("#shipping").hide();
+    });
+    $(".sub_nav li:nth-of-type(4)").click(function(){
+        $("#detail").hide();
+        $("#detail2").hide();
+        $("#review").hide();
+        $("#inquiry").show();
+        $("#shipping").hide();
+    });
+    $(".sub_nav li:last-of-type").click(function(){
+        $("#detail").hide();
+        $("#detail2").hide();
+        $("#review").hide();
+        $("#inquiry").hide();
+        $("#shipping").show();
+    });
+});
